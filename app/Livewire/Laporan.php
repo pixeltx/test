fb831a3a258be8f24edca60e5cff0b4624b8500a
wyd <arvinradithya.0@gmail.com>
2024-09-22 22:52:49 +0700

    Penambahan Menu Tipe Laporan

--- app/Livewire/Laporan.php
+++ app/Livewire/Laporan.php
@@ -4,14 +4,30 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Transaksi;
+use App\Models\User;
+use App\Models\Produk;
+use App\Models\Member;
 
 class Laporan extends Component
 {
+    public $selectedLaporan = '';
+
+    public function selectLaporan($laporan) {
+        $this->selectedLaporan = $laporan;
+    }
+
     public function render()
     {
         $semuaTransaksi = Transaksi::where('status', 'selesai')->get();
+        $semuaPetugas = User::where('role', 'kasir')->get();
+        $semuaProduk = Produk::all();
+        $semuaMember = Member::all();
+
         return view('livewire.laporan')->with([
-            'semuaTransaksi' => $semuaTransaksi
+            'semuaTransaksi' => $semuaTransaksi,
+            'semuaPetugas' => $semuaPetugas,
+            'semuaProduk' => $semuaProduk,
+            'semuaMember' => $semuaMember
         ]);
     }
 }
--- resources/views/livewire/laporan.blade.php
+++ resources/views/livewire/laporan.blade.php
@@ -4,6 +4,10 @@
         <div class="container">
             <div class="row mt-3">
                 <div class="col-12">
+                    <button wire:click="selectLaporan('transaksi')" class="btn {{ $selectedLaporan=='transaksi' ? 'btn-primary' : 'btn-outline-primary' }}">Laporan Transaksi</button>
+                    <button wire:click="selectLaporan('petugas')" class="btn {{ $selectedLaporan=='petugas' ? 'btn-primary' : 'btn-outline-primary' }}">Laporan Petugas</button>
+                    <button wire:loading class="btn btn-info">...</button>
+                    @if ($selectedLaporan == 'transaksi')
                     <div class="card border-primary">
                         <div class="card-body">
                             <h4 class="card-title">Laporan Transaksi</h4>
@@ -28,6 +32,30 @@
                             </table>
                         </div>
                     </div>
+                    @elseif ($selectedLaporan == 'petugas')
+                    <div class="card border-primary" style="width: 75vw;">
+                        <div class="card-body">
+                            <h4 class="card-title">Laporan Petugas</h4>
+                            <a href="{{ url('/cetak') }}" target="_blank">Cetak</a>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($semuaPetugas as $petugas)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $petugas->name }}</td>
+                                        <td>{{ $petugas->email }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
